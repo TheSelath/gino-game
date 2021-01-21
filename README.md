ba05161fccf8bf8450edfc3680bf7f40bf32de47
Thales Domingos <thales.domingos93@gmail.com>
2021-01-21 16:55:29 -0300

    projeto finalizado

---
+++ README.md
@@ -0,0 +1,7 @@
+# Game Dino
+
+Projeto realizado no Bootcamp da GFT na DIO
+
+O projeto contem, Javascript, CSS e HTML.
+
+Um clone com as funções principais do jogo do dino que funciona no chrome ao ficar sem internet. 
---
+++ js/script.js
@@ -0,0 +1,78 @@
+const dino = document.querySelector('.dino');
+const bg = document.querySelector('.bg');
+
+let isJumping = false;
+let isGameOver = false;
+let position = 0;
+
+function handleKeyUp(event){
+    if(event.keyCode === 32 || event.keyCode === 87 || event.keyCode === 38){
+        if(!isJumping){
+            jump();
+        }       
+    }
+}
+
+function jump(){
+    
+    isJumping = true;
+
+    let upInterval = setInterval(() =>{
+        if(position >= 150){
+            clearInterval(upInterval);
+            
+            //descer
+            let downInterval = setInterval(() =>{
+                if(position <= 0){
+                    clearInterval(downInterval);
+                    isJumping = false;
+                }else{
+                    position -= 20;
+                    dino.style.bottom = position +'px';
+                }                
+            }, 20);
+        }else{
+
+            //subir
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }     
+    }, 20);
+}
+
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    if(isGameOver) return;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    bg.appendChild(cactus);
+
+    let leftInterval = setInterval(() =>{
+        if(cactusPosition < -60 ){
+            clearInterval(leftInterval);
+            bg.removeChild(cactus);  
+        }else if (cactusPosition > 0 &&  cactusPosition < 60 && position < 60){
+
+            // game over
+
+            clearInterval(leftInterval);
+            isGameOver = true;
+            document.body.innerHTML = '<h1 class="game-over">Game Over</h1>';    
+        }else
+
+        {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }        
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+
+document.addEventListener('keyup', handleKeyUp);
